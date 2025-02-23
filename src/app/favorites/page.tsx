28b5f881e9420a811fefcfc8a810d2c5ad7a27db
anouermokrane <anouermokrane@gmail.com>
2025-02-23 14:55:51 +0100

    Remove unused imports and commented-out code from favorites page

--- src/app/favorites/page.tsx
+++ src/app/favorites/page.tsx
@@ -1,20 +1,9 @@
-import GridWrapper from "@/components/GridWrapper";
-import MovieCard, { Movie } from "@/components/MovieCard";
-import { getFavorites } from "@/lib/actions/favorite";
 import React from "react";
 
 export default async function page() {
-  const favIds = await getFavorites();
-
   return (
     <div>
       <h1>Favorites</h1>
-      <GridWrapper>
-        <h1>f</h1>
-        {/* {favorites.results.map((movie: Movie) => (
-          <MovieCard key={movie.id} movie={movie} />
-        ))} */}
-      </GridWrapper>
     </div>
   );
 }
