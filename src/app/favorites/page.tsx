699503e3f8f471aece49e36732dc902a73c2fe65
anouermokrane <anouermokrane@gmail.com>
2025-03-02 12:15:49 +0100

    Enhance favorites page to display a message when no favorite movies are present

--- src/app/favorites/page.tsx
+++ src/app/favorites/page.tsx
@@ -1,18 +1,26 @@
 import GridWrapper from "@/components/GridWrapper";
 import MovieCard, { Movie } from "@/components/MovieCard";
 import { getFavorites } from "@/lib/actions/favorite";
-import React from "react";
 
 export default async function page() {
   const data = await getFavorites();
+  const hasFavorites = data?.favorites && data.favorites.length > 0;
+
   return (
     <>
-      <h1 className="pb-6 text-lg">Favorites</h1>
-      <GridWrapper>
-        {data?.favorites.map((movie: Movie) => (
-          <MovieCard key={movie.id} movie={movie} />
-        ))}
-      </GridWrapper>
+      <h1 className="pb-6 text-lg font-bold">Your Favorites</h1>
+
+      {hasFavorites ? (
+        <GridWrapper>
+          {data.favorites.map((movie: Movie) => (
+            <MovieCard key={movie.id} movie={movie} />
+          ))}
+        </GridWrapper>
+      ) : (
+        <div className="flex items-center justify-center h-64">
+          <p className="text-gray-500">You have no favorite movies yet.</p>
+        </div>
+      )}
     </>
   );
 }
